68f8f045f89815d2bc0873633a5d91f1da2b6d23
Thirvusoft <frappe@ip-172-26-14-204.ap-south-1.compute.internal>
2024-05-04 09:04:46 +0000

    print_format

--- chitrafabrics/chitrafabrics/utils/py/batch.py
+++ chitrafabrics/chitrafabrics/utils/py/batch.py
@@ -3,9 +3,9 @@ from frappe.model.naming import parse_naming_series, make_autoname, revert_serie
 
 def batch_id_naming(doc, action):
     if doc.item:
-        doc.name = make_autoname(f"{doc.item}-.#")
+        doc.name = make_autoname(f"{doc.item}-B.#")
         doc.batch_id = doc.name
 
 def batch_id_trash(doc, action):
     if doc.item:
-        revert_series_if_last(f"{doc.item}-.#", doc.name)
+        revert_series_if_last(f"{doc.item}-B.#", doc.name)
