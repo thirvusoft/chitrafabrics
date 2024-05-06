69d0d3a2466f5b6a568643494a2b39efce12d413
creative-paramu <pparameshwari@thirvusoft.in>
2024-05-06 12:06:49 +0530

    batch_price

--- chitrafabrics/chitrafabrics/utils/py/batch.py
+++ chitrafabrics/chitrafabrics/utils/py/batch.py
@@ -9,3 +9,24 @@ def batch_id_naming(doc, action):
 def batch_id_trash(doc, action):
     if doc.item:
         revert_series_if_last(f"{doc.item}-B.#", doc.name)
+
+
+def price_updation(doc, action):
+    if doc.docstatus == 1:
+        for entry in doc.entries:
+            if entry.batch_no:
+                batch=frappe.get_doc("Batch", entry.batch_no)
+                if batch.item == doc.item_code and entry.batch_no == batch.name:
+                    branch = frappe.db.get_value("Warehouse", entry.warehouse, "custom_branch")
+                    item_price = frappe.db.get_list("Item Price", 
+                                                         filters={"item_code": doc.item_code, 
+                                                                  "custom_branch": branch, 
+                                                                  "price_list": "Retail Selling", 
+                                                                  "selling": 1}, 
+                                                         fields=["price_list_rate"])
+                    if item_price:
+                            price_list_rate = item_price[0].get("price_list_rate")
+                            rounded_price = round(price_list_rate)
+                            batch.custom_item_price = rounded_price
+                            batch.save()
+
