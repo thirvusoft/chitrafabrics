a16f6ce32dc3d4807cf3d79485fdd985a440f95f
Nandhinidevi123 <Nandhinidevi123>
2024-05-16 17:17:41 +0530

    stock entry set account data

--- chitrafabrics/hooks.py
+++ chitrafabrics/hooks.py
@@ -123,6 +123,9 @@ doctype_js = {"Sales Invoice" : "chitrafabrics/chitrafabrics/chitrafabrics/utils
 # Hook on document methods and events
 
 doc_events = {
+    "Stock Entry" : {
+        "on_update":"chitrafabrics.chitrafabrics.utils.py.stock_entry.fetchdata"
+	},
 	"Item": {
         "on_update": "chitrafabrics.chitrafabrics.utils.py.purchase_invoice.new_rate",
 		"autoname": "chitrafabrics.chitrafabrics.utils.py.item.item_name",
