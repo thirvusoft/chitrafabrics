63756c3b33d01d7fdaa9032100ae18dc326c45b8
root <root@ip-172-26-14-204.ap-south-1.compute.internal>
2024-05-18 07:05:38 +0000

    discount doctype

--- chitrafabrics/chitrafabrics/utils/py/stock_entry.py
+++ chitrafabrics/chitrafabrics/utils/py/stock_entry.py
@@ -1,8 +1,11 @@
+import frappe
+
 def fetchdata(doc , events):
     if(doc.stock_entry_type == "Opening Entry"):
         doc.is_opening = "Yes"
         for i in doc.items:
             i.expense_account = "Temporary Opening - CF"
+        frappe.msgprint("Difference Account set to Temporary Opening - CF")
    
 
 
