bd79a1a187fd0f82225370de8cb96f860fab42fa
root <root@ip-172-26-14-204.ap-south-1.compute.internal>
2024-05-18 07:29:48 +0000

    buf fix in stock entry

--- chitrafabrics/chitrafabrics/utils/py/stock_entry.py
+++ chitrafabrics/chitrafabrics/utils/py/stock_entry.py
@@ -5,6 +5,7 @@ def fetchdata(doc , events):
         doc.is_opening = "Yes"
         for i in doc.items:
             i.expense_account = "Temporary Opening - CF"
+            i.branch = doc.branch
         frappe.msgprint("Difference Account set to Temporary Opening - CF")
    
 
